6ebabd25b89f42a0be26741bd6c3b89eb5c97037
Ralpek <drakalbert11@gmail.com>
2026-05-01 13:00:00 +0200

    estructura del front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Alumnos } from './pages/alumnos/alumnos';
+import { Libros } from './pages/libros/libros';
+import { Prestamos } from './pages/prestamos/prestamos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'alumnos', component: Alumnos },
+  { path: 'libros', component: Libros },
+  { path: 'prestamos', component: Prestamos },
+  { path: '**', redirectTo: '/dashboard' } 
+];

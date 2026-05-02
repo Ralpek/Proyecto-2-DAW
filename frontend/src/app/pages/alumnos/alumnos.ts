bd91e174a74be8a0f543b4e6b25b534ae7fb7941
Ralpek <drakalbert11@gmail.com>
2026-05-02 11:00:00 +0200

    formulario añadir alumno

--- frontend/src/app/pages/alumnos/alumnos.ts
+++ frontend/src/app/pages/alumnos/alumnos.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 // Definimos la estructura exacta que tendrá un alumno
 export interface Alumno {
@@ -13,7 +14,7 @@ export interface Alumno {
 @Component({
   selector: 'app-alumnos',
   standalone: true,
-  imports: [CommonModule], // Importamos CommonModule para poder usar directivas visuales
+  imports: [CommonModule, FormsModule], // Importamos CommonModule para poder usar directivas visuales
   templateUrl: './alumnos.html',
   styleUrl: './alumnos.css'
 })
@@ -24,4 +25,32 @@ export class Alumnos {
     { id: 2, nombre: 'Carlos', apellidos: 'Martínez Ruiz', tramo: 'II', bilingue: 'N' },
     { id: 3, nombre: 'Lucía', apellidos: 'Fernández Díaz', tramo: 'Ninguno', bilingue: 'S' }
   ];
+  // --- LÓGICA DEL MODAL ---
+
+  // Controla si el modal se ve o no
+  mostrarModal: boolean = false;
+
+  // Objeto temporal para guardar los datos del formulario
+  nuevoAlumno: Alumno = { id: 0, nombre: '', apellidos: '', tramo: 'Ninguno', bilingue: 'N' };
+
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+    // Limpiamos el formulario para la próxima vez que se abra
+    this.nuevoAlumno = { id: 0, nombre: '', apellidos: '', tramo: 'Ninguno', bilingue: 'N' };
+  }
+
+  guardarAlumno() {
+    // Le asignamos un ID falso (el tamaño actual de la lista + 1)
+    this.nuevoAlumno.id = this.alumnos.length + 1;
+
+    // Metemos el nuevo alumno en la tabla
+    this.alumnos.push({ ...this.nuevoAlumno });
+
+    // Cerramos la ventana
+    this.cerrarModal();
+  }
 }

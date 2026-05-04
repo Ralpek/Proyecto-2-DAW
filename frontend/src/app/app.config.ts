1e5a2ff7af4a698f7c0d15d6de80ddfe89a9b7e0
Ralpek <drakalbert11@gmail.com>
2026-05-04 12:00:00 +0200

    cambios para que se vea

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,4 +1,7 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+/*// @ts-ignore
+import 'zone.js';
+*/
+import { ApplicationConfig, /*provideZoneChangeDetection*/ } from '@angular/core';
 import { provideRouter } from '@angular/router';
 // herramienta para hacer peticiones HTTP
 import { routes } from './app.routes';
@@ -6,8 +9,8 @@ import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    /*provideZoneChangeDetection({ eventCoalescing: true }),*/ 
     provideRouter(routes), 
-    provideHttpClient()
+    provideHttpClient(),
   ] 
 };

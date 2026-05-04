6c59f706537d45394506a2f2f805108301729513
Ralpek <drakalbert11@gmail.com>
2026-05-04 13:00:00 +0200

    misma url

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,15 +2,20 @@
 import 'zone.js';
 */
 import { ApplicationConfig, /*provideZoneChangeDetection*/ } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 // herramienta para hacer peticiones HTTP
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http'; 
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    /*provideZoneChangeDetection({ eventCoalescing: true }),*/ 
-    provideRouter(routes), 
+    /*provideZoneChangeDetection({ eventCoalescing: true }),*/
+    provideRouter(
+      routes,
+      withRouterConfig({
+        onSameUrlNavigation: 'reload'
+      })
+    ),
     provideHttpClient(),
-  ] 
+  ]
 };
